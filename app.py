efb6a96565aa080ca876367d3e2bc6b5f1d2b94a
DongJunHan <gamedokdok@naver.com>
2023-09-29 00:52:15 +0900

    feat: log 파일 작성하는 함수 추가

--- app.py
+++ app.py
@@ -15,13 +15,25 @@ class CustomFlaskApp(Flask):
 
     def get_yaml_data(self):
         yaml_data = self.load_yaml()
+        self.write_log_file(yaml.dump(yaml_data))
         self.base_url = yaml_data['base_url']
+
+    def write_log_file(self, *args, **kwargs):
+        for arg in args:
+            str_format = ''
+            str_format += arg
+            with open('log.log', 'a') as f:
+                f.write(f"[@@@] {str_format}")
+        for key, value in kwargs.items():
+            str_format = ''
+            str_format += f"{key}: {value}"
+            with open('log.log', 'a') as f:
+                f.write(f"[@@@] {str_format}")
 
 app = CustomFlaskApp(__name__)
 GET = 'GET'
 POST = 'POST'
 @app.route("/", methods = [GET, POST])
 def root():
-    print(f"base_url: {app.base_url}")
+    app.write_log_file(f"base_url: {app.base_url}")
     return render_template('index.html')
-    
